ba39dbf9bf8f674dcc96ce10f1113788695c7631
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-20 17:49:39 +0800

    create temporary playlist screen

---
+++ screens/PlaylistsScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector } from "react-redux";
+import { RootState } from "../reducers/rootReducer";
+
+const PlaylistsScreen = () => {
+  const { videos } = useSelector((state: RootState) => state.queueState);
+
+  return (
+    <SafeAreaView>
+      <Text>Playlists Screen</Text>
+      <FlatList
+        data={videos}
+        renderItem={(item) => <Text>{item.item}</Text>}
+        keyExtractor={(item, index) => index + item}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default PlaylistsScreen;
