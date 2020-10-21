a3640b65261c260a8605843e3b963dd24acc9d27
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-21 18:16:46 +0800

    convert to react query

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,103 +1,69 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
-  FlatList,
   NativeSyntheticEvent,
   TextInput,
   TextInputSubmitEditingEventData,
 } from "react-native";
+import { Portal } from "react-native-portalize";
 import { SafeAreaView } from "react-native-safe-area-context";
-import YoutubePlayer from "react-native-youtube-iframe";
-import useSWR, { useSWRInfinite, cache } from "swr";
-import { VideoItem } from "../components/VideoItem";
-import { Item, SearchResult } from "../types";
+import { useInfiniteQuery } from "react-query";
+import { QueueBottomSheet } from "../components/QueueBottomSheet";
+import { SearchResultsList } from "../components/SearchResultsList";
+import { SearchResult } from "../types";
 
 const API_URL =
   "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10";
 const API_TOKEN = "AIzaSyCrtUmM_inD-Kh0SV2M-ZVWFLzdbBorx0w";
 
-const fetcher = (...args: any) => {
-  console.log("fetching");
-  const key = `&key=${API_TOKEN}`;
-  const q = `&q=${args[2]}`;
-  let nextPage = "";
-  if (nextPage !== args[1]) {
-    nextPage = `&pageToken=${args[1]}`;
-  }
-  const url = API_URL + key + q + nextPage;
-  console.log(url);
-  const fetching = fetch(url).then((res) => res.json());
-
-  return fetching;
-};
-
 const HomeScreen = () => {
-  const [playing, setPlaying] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [items, setItems] = useState<Item[]>([]);
   const [shouldFetch, setShouldFetch] = useState(false);
-
-  const getKey = useCallback(
-    (pageIndex: number, previousPageData: SearchResult | null) => {
-      if (!shouldFetch) return null;
-
+  const {
+    status,
+    data,
+    error,
+    isFetching,
+    isFetchingMore,
+    fetchMore,
+    canFetchMore,
+    remove,
+  } = useInfiniteQuery<SearchResult>(
+    "search",
+    async (key, nextPageToken = "") => {
+      console.log("fetching");
       if (searchValue === "") {
         return null;
       }
-
-      if (previousPageData && !previousPageData.items) {
-        return null;
+      const token = `&key=${API_TOKEN}`;
+      const q = `&q=${searchValue}`;
+      let nextPage = "";
+      if (nextPage !== nextPageToken) {
+        nextPage = `&pageToken=${nextPageToken}`;
       }
-      // first page, we don't have `previousPageData`
-      if (pageIndex === 0) {
-        return ["search", "", searchValue];
-      }
+      const url = API_URL + token + q + nextPage;
+      console.log(url);
+      const fetching = fetch(url).then((res) => res.json());
 
-      // add the nextPageToken
-      return ["search", previousPageData?.nextPageToken, searchValue];
+      return fetching;
     },
-    [searchValue, shouldFetch]
-  );
-
-  const { data, size, setSize, error } = useSWRInfinite<SearchResult>(
-    getKey,
-    fetcher
-  );
-
-  const onStateChange = useCallback((state) => {
-    if (state === "ended") {
-      setPlaying(false);
+    {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      getFetchMore: (lastGroup) => lastGroup?.nextPageToken,
     }
-  }, []);
-
-  const togglePlaying = useCallback(() => {
-    setPlaying((prev) => !prev);
-  }, []);
+  );
 
   const handleSubmit = (
     e: NativeSyntheticEvent<TextInputSubmitEditingEventData>
   ) => {
-    setItems([]);
+    remove();
     setShouldFetch(true);
     setSearchValue(e.nativeEvent.text);
   };
 
   const handleEndReached = () => {
-    setSize(size + 1);
-    setShouldFetch(true);
+    fetchMore();
   };
-
-  useEffect(() => {
-    if (data || error) {
-      setShouldFetch(false);
-    }
-    if (data && data.length && !error) {
-      const newData = data[data.length - 1];
-      if (newData && newData.items) {
-        console.log("adding new items");
-        setItems((prevItems) => [...prevItems, ...newData.items]);
-      }
-    }
-  }, [data, error, setShouldFetch, shouldFetch]);
 
   return (
     <SafeAreaView>
@@ -105,14 +71,13 @@ const HomeScreen = () => {
         style={{ height: 40, borderWidth: 1 }}
         onSubmitEditing={handleSubmit}
       />
-
-      <FlatList
-        data={items}
-        renderItem={(item) => <VideoItem item={item.item} />}
-        keyExtractor={(_item, index) => String(index)}
-        onEndReached={handleEndReached}
+      <SearchResultsList
+        data={data || []}
+        handleEndReached={handleEndReached}
       />
-      {/* <Button disabled={!size} title="Load More" onPress={handleEndReached} /> */}
+      <Portal>
+        <QueueBottomSheet />
+      </Portal>
     </SafeAreaView>
   );
 };
