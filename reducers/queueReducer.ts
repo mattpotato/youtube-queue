1ba60d32ac2b8c06b4b66edda94f1f22260c13ee
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-21 18:16:28 +0800

    add more actions

--- reducers/queueReducer.ts
+++ reducers/queueReducer.ts
@@ -1,25 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { ViewPagerAndroidBase } from "react-native";
+import { Item } from "../types";
 
 interface QueueState {
-  videos: string[];
+  videos: Item[];
+  currentVideoIndex: number;
 }
 
 const initialState: QueueState = {
   videos: [],
+  currentVideoIndex: 0,
 };
+
 const queueSlice = createSlice({
   name: "queue",
   initialState,
   reducers: {
     addToQueue: (state, action) => {
-      state.videos.push(action.payload);
+      const index = state.videos.findIndex(
+        (item) => item.id.videoId === action.payload.id.videoId
+      );
+      if (index === -1) {
+        state.videos.push(action.payload);
+      }
     },
     deleteQueue: (state) => {
       state.videos = [];
     },
+    playNextInQueue: (state) => {
+      // increment the index
+      if (state.currentVideoIndex < state.videos.length - 1) {
+        state.currentVideoIndex += 1;
+      }
+    },
+    playPreviousInQueue: (state) => {
+      // decrement the index
+      if (state.currentVideoIndex > 0) {
+        state.currentVideoIndex -= 1;
+      }
+    },
+    setCurrentVideoIndex: (state, action) => {
+      state.currentVideoIndex = action.payload;
+    },
   },
 });
 
-export const { addToQueue } = queueSlice.actions;
+export const {
+  addToQueue,
+  deleteQueue,
+  playNextInQueue,
+  playPreviousInQueue,
+  setCurrentVideoIndex,
+} = queueSlice.actions;
 
 export default queueSlice.reducer;
