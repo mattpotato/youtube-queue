525554703542ba40ae58c4dc7dd6c35d898947f2
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-21 18:21:02 +0800

    remove unused

--- screens/PlaylistsScreen.tsx
+++ screens/PlaylistsScreen.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useSelector } from "react-redux";
-import { RootState } from "../reducers/rootReducer";
 
 const PlaylistsScreen = () => {
-  const { videos } = useSelector((state: RootState) => state.queueState);
-
   return (
     <SafeAreaView>
       <Text>Playlists Screen</Text>
-      <FlatList
-        data={videos}
-        renderItem={(item) => <Text>{item.item}</Text>}
-        keyExtractor={(item) => item.id.videoId}
-      />
     </SafeAreaView>
   );
 };
