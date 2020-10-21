c922c1457736d3a1672118b0b6745fbce92f2fd4
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-21 18:06:52 +0800

    create searchresultslist

---
+++ components/SearchResultsList.tsx
@@ -0,0 +1,33 @@
+import React, { useMemo } from "react";
+import { FlatList, View, Text } from "react-native";
+import { Item, SearchResult } from "../types";
+import { VideoItem } from "./VideoItem";
+
+interface SearchResultsListProps {
+  data: SearchResult[];
+  handleEndReached:
+    | ((info: { distanceFromEnd: number }) => void)
+    | null
+    | undefined;
+}
+
+export const SearchResultsList: React.FC<SearchResultsListProps> = ({
+  data,
+  handleEndReached,
+}) => {
+  const itemsData = useMemo(() => {
+    let items: Item[] = [];
+    data?.forEach((searchResult) =>
+      searchResult?.items.map((item) => items.push(item))
+    );
+    return items;
+  }, [data]);
+  return (
+    <FlatList
+      data={itemsData}
+      renderItem={({ item }) => <VideoItem item={item} />}
+      keyExtractor={(item, index) => item.id.videoId + index}
+      onEndReached={handleEndReached}
+    />
+  );
+};
