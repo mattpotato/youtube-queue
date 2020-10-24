7450811e0ea6ba58b079ee13886456ec5f426793
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-24 15:22:08 +0800

    add separator to flatlist

--- components/SearchResultsList.tsx
+++ components/SearchResultsList.tsx
@@ -28,6 +28,16 @@ export const SearchResultsList: React.FC<SearchResultsListProps> = ({
       renderItem={({ item }) => <VideoItem item={item} />}
       keyExtractor={(item, index) => item.id.videoId + index}
       onEndReached={handleEndReached}
+      ItemSeparatorComponent={() => (
+        <View
+          style={{
+            height: 0.5,
+            width: "100%",
+            backgroundColor: "#000",
+            opacity: 0.2,
+          }}
+        />
+      )}
     />
   );
 };
