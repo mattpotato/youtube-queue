3657bab4345f24731f921ac457086838c75ba4d4
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-25 19:23:17 +0800

    add playlist state

--- reducers/rootReducer.ts
+++ reducers/rootReducer.ts
@@ -1,7 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import playlistReducer from "./playlistReducer";
 import queueReducer from "./queueReducer";
 
-const rootReducer = combineReducers({ queueState: queueReducer });
+const rootReducer = combineReducers({
+  queueState: queueReducer,
+  playlistState: playlistReducer,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
 
