373ac07b77cffe5ff95e0518c8620364458b9cdc
matt011 <38515217+matt011@users.noreply.github.com>
2020-10-29 17:58:00 +0800

    create save screen

---
+++ screens/SaveScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+interface SaveScreenProps {}
+
+const SaveScreen: React.FC<SaveScreenProps> = ({}) => {
+  return (
+    <View>
+      <Text>Save Screen</Text>
+    </View>
+  );
+};
+
+export default SaveScreen;
